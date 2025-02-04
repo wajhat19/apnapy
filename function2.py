3c8f684ddb0d8bef174888801137d6c6b875dc63
wajhat19 <wajhatpathan9@gmail.com>
2025-02-04 20:39:28 +0530

    1

---
+++ function2.py
@@ -0,0 +1,9 @@
+cities=["hubli","dharwad","bnaglore","belgaum"]
+heros=["ironman","batman","superman","spiderman","hulk"]
+
+def print_list(list):
+    for item in list:
+        print(item, end=" ")
+
+print_list(heros)    
+print_list(cities)    
---
+++ functions.py
@@ -0,0 +1,8 @@
+cities=["hubli","dharwad","bnaglore","belgaum"]
+heros=["ironman","batman","superman","spiderman","hulk"]
+
+def print_len(list):
+    print(len(list))
+
+print_len(cities) 
+print_len(heros)   
